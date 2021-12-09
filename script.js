d266e6d769470635aee3c06ba0b27ddffcccf6ac
dominici94 <mattiadominici94@gmail.com>
2021-12-09 15:21:37 +0100

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+// Ciao ragazzi,
+// Esercizio di oggi: Vue Email List
+// nome repo: vue-email-List
+// Descrizione
+// Attrvaerso l'apposita API di Boolean :api:
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati. (modificato) 
