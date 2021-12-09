47eb35550ec20da1502c01f4adf4e5499df6716c
dominici94 <mattiadominici94@gmail.com>
2021-12-09 17:37:54 +0100

    esercizio

--- script.js
+++ script.js
@@ -7,3 +7,30 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati. (modificato) 
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        mail: [],
+    },
+    methods:{
+
+    },
+    created(){
+        for(let i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) =>{
+                // handle success
+                // console.log(response.data.response);
+                // this.mail = response.data.response;
+                this.mail.push(response.data.response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+        }
+        console.log(this.mail);
+        
+    }
+});
